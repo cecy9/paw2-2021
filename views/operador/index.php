49879b89c88e066799eb53e49200adac82510891
cecy9 <88103514+cecy9@users.noreply.github.com>
2021-08-30 23:01:20 -0600

    paw

--- views/operador/index.php
+++ views/operador/index.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Operador</title>
+    <title>Administrador</title>
 </head>
 <body>
     <h3><b>Bienvenido/a: <?php echo $user; ?> </b></h3>
