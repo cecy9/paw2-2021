9ce0c699216fee0fdfe510c5b87be87724547a75
cecy9 <valladarescecy56@gmail.com>
2021-09-01 11:51:40 -0600

    update

--- views/admin/index.php
+++ views/admin/index.php
@@ -14,6 +14,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrador</title>
+    <!--CSS -->
+    <link rel="stylesheet" href="../../public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../public/css/bootstrap-theme.css">
+    <link rel="stylesheet" href="../../public/css/estilo.css">
+    <!--JS -->
+    <script src="../../public/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="../../public/js/jquery-1.9.1.min.js"></script>
+    <script src="../../public/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/05f4903dc9.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <?php include 'navbar/navbar.php'; ?>
